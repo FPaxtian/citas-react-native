5519e9ad083b8efce836fae4ab599bc9c4e73a0e
Francisco Paxtian <fizquierdo@quaxar.com>
2021-12-08 23:50:55 -0600

    add css_exaple

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, FlatList } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 export default function App() {
+
+  const [state, setState] = useState([
+    { id: 1, name: 'Francisco' },
+    { id: 1, name: 'Javier' },
+    { id: 1, name: 'Jr' }
+  ])
+
   return (
-    <View style={tw`p-4 android:pt-2 bg-blue-700 flex-row max-w-7xl mx-auto w-full`}>
-      <Text style={tw`text-center text-white font-bold text-2xl w-full`}>Administrador de citas</Text>
+    <View style={tw`max-w-7xl mx-auto w-full`}>
+
+      <Text style={tw`android:pt-2 text-center bg-blue-700 p-4 text-white font-bold text-2xl w-full`}>Administrador de citas</Text>
+
+      <FlatList
+        data={state}
+        renderItem={({ item }) => (
+          <View>{item.name}</View>
+        )}
+        keyExtractor={state => state.id}
+      />
+
     </View>
   );
 }
